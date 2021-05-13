4f7b41721e465af610c2de29255f57d5d81ba4c5
alexandre <tconect.telecom@gmail.com>
2021-05-13 17:41:05 -0300

    first commit

---
+++ src/router/routes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import {Router} from  "express";
+const routes = Router();
+
+
+
+
+
+export { routes };
---
+++ src/server.ts
@@ -0,0 +1,42 @@
+// import { http } from "./http";
+import express, { application } from "express";
+import { createServer} from "http";
+import { Server,Socket } from "socket.io";
+import path from "path";
+import {routes} from "./router/routes";
+const app = express();
+
+// usando template engine
+app.set('view engine',  'ejs')
+
+// Mudar a localização da pasta views
+app.set('views', path.join(__dirname, 'views'))
+
+
+
+//habilitar arquivos statics
+app.use(express.static(__dirname + "/public"))
+
+    app.use(express.static(
+    path.resolve(__dirname, '..', '/public')))
+
+
+// usar o req.body
+app.use(express.urlencoded({ extended: true }))
+
+// routes
+app.use(routes)
+
+routes.get("/pages/blog", (request,response) => {
+      return response.render("../views/index");
+});
+const http = createServer(app);//criando protocolo http
+const io = new Server(http);//criando protocolo ws
+
+ io.on("connection",(socket ) => {
+     console.log("se conectou,socket.id");
+});
+
+http.listen(8080,() => {
+    console.log(' sevidor rodando na url  localhost:8080');
+});
